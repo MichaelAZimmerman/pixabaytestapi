4ac6326eee4449fc54b095a893eab64cb94c96b3
Michael Zimmerman <mikezne@gmail.com>
2021-07-02 11:53:50 -0500

    makes store. sets up index.js and starts App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import "./App.css";
+import { clearFavorites, clearUser, clearSearch } from "./redux/actions";
 import Login from "./components/Login";
 import Search from "./components/Search";
 import Favorites from "./components/Favorites";
@@ -12,8 +13,77 @@ import {
   Switch,
 } from "react-router-dom";
 
-function App() {
-  return <div></div>;
+function App({ username, clearFavorites, clearSearch, clearUser }) {
+  return (
+    <Router>
+      <header className="flex-wrap">
+        {username ? (
+          <h3>{username} is currently logged in.</h3>
+        ) : (
+          <h3>Please log in to continue.</h3>
+        )}
+      </header>
+
+      <nav className="flex-wrap">
+        {!username && (
+          <NavLink
+            activeClassName="active"
+            className="link text-center"
+            to="/login"
+            onClick={() => {
+              clearFavorites();
+              clearSearch();
+              clearUser();
+            }}
+          >
+            Login
+          </NavLink>
+        )}
+        {username && (
+          <>
+            <NavLink
+              activeClassName="active"
+              className="link text-center"
+              to="/search"
+            >
+              Search
+            </NavLink>
+
+            <NavLink
+              activeClassName="active"
+              className="link text-center"
+              to="/favorites"
+            >
+              Favorites
+            </NavLink>
+            <NavLink
+              className="link text-center"
+              to="/login"
+              onClick={() => {
+                clearFavorites();
+                clearUser();
+                clearSearch();
+              }}
+            >
+              Logout
+            </NavLink>
+          </>
+        )}
+      </nav>
+    </Router>
+  );
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    username: state.user.username,
+  };
+}
+
+const mapDispatchToProps = {
+  clearFavorites,
+  clearUser,
+  clearSearch,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
