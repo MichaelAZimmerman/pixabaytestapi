25a6d706f7f535a0818fa83bdcb55c0aa1d9f46f
Michael Zimmerman <mikezne@gmail.com>
2021-07-02 15:35:18 -0500

    fixes add favorite button

--- src/redux/actions/favoritesActions.js
+++ src/redux/actions/favoritesActions.js
@@ -2,8 +2,8 @@ export const ADD_FAVORITE = "Add Favorite";
 export const DELETE_FAVORITE = "Delete Favorite";
 export const CLEAR_FAVORITES = "Clear Favorites";
 
-export function addFavorite(gif) {
-  return { type: ADD_FAVORITE, gif };
+export function addFavorite(pic) {
+  return { type: ADD_FAVORITE, pic };
 }
 
 export function deleteFavorite(id) {
